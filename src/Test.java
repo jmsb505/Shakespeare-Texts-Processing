70f1cb0ea92aed3aa501542aa4c82f9ba73a0e1b
Juan Martin Sanchez Bardellini <sjuan0867@gmail.com>
2022-02-09 14:15:41 -0500

    Lectura de textos a un array de strings

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        Path p= Paths.get("src/Shakespeare.txt");
+
+
+
+
+    }
+
+}
+
