e453a6bd17b99e259ae57639d0af6aa7488777f0
Carlos Montiel <carlosmontiel0204@gmail.com>
2022-02-10 10:52:48 -0500

    Secuencial mas rapido

--- src/Test.java
+++ src/Test.java
@@ -12,6 +12,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -24,14 +26,33 @@ public class Test {
         Texto t= new Texto(obras[0]);
         Texto t2= new Texto(obras[1]);
         int count=0;
-        ExecutorService executorService = Executors.newCachedThreadPool();//newFixedThreadPool(4);
+        ExecutorService executorService = Executors.newFixedThreadPool(38);
+        Long tpara=System.currentTimeMillis();
         for(int i=0;i<obras.length;i++)
         {
           executorService.execute(new Texto(obras[i]));
-          System.out.println("Existen "+Thread.activeCount()+" threads (incluyendo el main)");
+          System.out.println("Existen paralelamente "+Thread.activeCount()+" threads (incluyendo el main)");
         }
-        
         executorService.shutdown();
+        try {
+            executorService.awaitTermination(10_000L, TimeUnit.MILLISECONDS );
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+       
+
+       
+        
+        //Secuencial
+        if(executorService.isTerminated())
+        {
+        System.out.println("Existen secuencialmente "+Thread.activeCount()+" threads (incluyendo el main)");
+        Texto textfinal=new Texto(obras);
+        Long tfinal=System.currentTimeMillis();
+        textfinal.setInputTextos(obras);
+        System.out.println("Tiempo secuencial "+ (System.currentTimeMillis()-tfinal)+ " ms");
+        }
 
 
 
