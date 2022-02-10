57127155036ab593d312e47ec2ac4b0205a3a982
Carlos Montiel <carlosmontiel0204@gmail.com>
2022-02-09 22:40:10 -0500

    A mimir

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -19,7 +22,16 @@ public class Test {
         String wholetxt= Files.lines(Paths.get("Shakespeare.txt")).collect(Collectors.joining());
         String[] obras = wholetxt.split("@");
         Texto t= new Texto(obras[0]);
-        System.out.println(t.getMap());
+        Texto t2= new Texto(obras[1]);
+        int count=0;
+        ExecutorService executorService = Executors.newCachedThreadPool();//newFixedThreadPool(4);
+        for(int i=0;i<obras.length;i++)
+        {
+          executorService.execute(new Texto(obras[i]));
+          System.out.println("Existen "+Thread.activeCount()+" threads (incluyendo el main)");
+        }
+        
+        executorService.shutdown();
 
 
 
--- src/Texto.java
+++ src/Texto.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,20 +7,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class Texto {
+public class Texto implements Runnable{
+    private String[] libros;
+    private String individual;
+    private Map<String,Long> map= new TreeMap<>();
+    private Long tiempo;
 
     public Texto(String[] s) throws IOException {
+        this.libros=s;
         setInputTextos(s);
     }
 
     public Texto(String s) throws IOException {
-        setInputTexto(s);
+        this.individual=s;
+        //setInputTexto(s);
     }
 
-    Map<String,Long> map= new TreeMap<>();
+    
 
     public void setInputTextos(String[] str) throws IOException {
         Pattern pattern= Pattern.compile("");
@@ -36,8 +46,31 @@ public class Texto {
                 .filter(pattern1.asPredicate())
                 .collect(Collectors.groupingBy(String::toUpperCase, TreeMap::new, Collectors.counting()));
     }
+        public void setInputTexto2(String str) throws IOException {
+        Pattern pattern1 = Pattern.compile("[A-Z]");
+        map= Arrays.stream(str.split(""))
+                .map(String::toUpperCase)
+                .filter(pattern1.asPredicate())
+                .collect(Collectors.groupingBy(String::toUpperCase, TreeMap::new, Collectors.counting()));
+        
+    }
 
     public Map<String, Long> getMap() {
         return map;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Long t1=System.currentTimeMillis();
+            setInputTexto2(individual);
+            tiempo=System.currentTimeMillis()-t1;
+            System.out.println("Thread"+Thread.currentThread().getName()+" se demoro "+ tiempo);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Texto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    
     }
 }
